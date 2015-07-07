52aba7f8ae71dcfbe24908d8e0aeb823bc984171
Keith Ch <AiRWaLK3R@AiRWaLK3R.local>
2015-07-07 08:44:27 -0700

    Added partial files for home, dashboard, and trip

--- client/js/modules/module.js
+++ client/js/modules/module.js
@@ -8,6 +8,15 @@ app.config(function($routeProvider) {
 	.when('/login', {
 		templateUrl: 'partials/login.html'
 	})
+	.when('/home', {
+		templateUrl: 'partials/home.html'
+	})
+	.when('/dashboard', {
+		templateUrl: 'partials/dashboard.html'
+	})
+	.when('/trip', {
+		templateUrl: 'partials/trip.html'
+	})
 	.otherwise({
 		redirectTo: '/'
 	});
