15b1bdfb2b5a24ba4f885392c44b0c72c3322359
Jens Townsdin <jenst44@gmail.com>
2015-07-07 19:07:31 -0700

    Front end tweeks

--- client/js/controllers/messages.js
+++ client/js/controllers/messages.js
@@ -2,6 +2,6 @@ app.controller('MessagesController', function($location, $rootScope) {
 	// this becomes that so that we have access to it in the functions
 	var that = this;
 	that.addmessage = function() {
-		console.log(that.new_message.message);
+		console.log(that.new_message.messagem);
 	}
 });
