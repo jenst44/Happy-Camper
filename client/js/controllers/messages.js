87cb22a061573bc6b5af2a7e37b88e8ff42b20c3
Jens Townsdin <jenst44@gmail.com>
2015-07-07 18:39:10 -0700

    Added messages controller

---
+++ client/js/controllers/messages.js
@@ -0,0 +1,7 @@
+app.controller('MessagesController', function($location, $rootScope) {
+	// this becomes that so that we have access to it in the functions
+	var that = this;
+	that.addmessage = function() {
+		console.log(that.new_message.message);
+	}
+});
