2d99a52d9c4869029031f10283345d1fe85e5bae
Jens Townsdin <jenst44@gmail.com>
2015-07-09 19:37:56 -0700

    Edited front end as well as weather api

--- client/js/models/weather.js
+++ client/js/models/weather.js
@@ -1,8 +1,8 @@
 app.factory('WeatherFactory', function($http) {
 	var factory = {};
 
-	factory.show = function(callback) {
-		$http.get("http://api.openweathermap.org/data/2.5/forecast?q=Seattle,us&mode=json&cnt=5&units=imperial&APPID=c1cc9d142bd3d306226d1e3f8d125cad")
+	factory.show = function(city, callback) {
+		$http.get("http://api.openweathermap.org/data/2.5/forecast?q="+city+",us&mode=json&cnt=5&units=imperial&APPID=c1cc9d142bd3d306226d1e3f8d125cad")
 		.success(function(res){
 			callback(res);
 		})
