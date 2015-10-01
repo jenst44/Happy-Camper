aa9c9d315ff034739fb28c28f62ee19201508ce7
Jens Townsdin <jenst44@gmail.com>
2015-10-01 16:41:15 -0700

    Update mongoose.js
    database

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 // connect to the database
 // mongoose.connect('mongodb://localhost/happy_campers');
-mongoose.connect('mongodb://heroku_d9ggsqp7:6lkfq7thue638ango8pgmvdv2l@ds031183.mongolab.com:31183/heroku_d9ggsqp7');
+mongoose.connect('database');
 // specify the path to all of the models
 var models_path = __dirname + '/../server/models'
 // read all of the files in the models_path and for each one check if it is a javascript file before requiring it
